cd003cee55cdfd3d333ff85ce747bb4c7af07e90
VS <VIVEKLUCKY249@users.noreply.github.com>
2017-02-11 03:46:00 +0530

    Create EnableModCmdSequence.cmd

---
+++ EnableModCmdSequence.cmd
@@ -0,0 +1,6 @@
+php bin/magento cache:clean
+php bin/magento cache:flush
+php bin/magento indexer:reindex
+php bin/magento module:enable MagePsycho_Easypathhints --clear-static-content
+php bin/magento setup:upgrade
+php bin/magento setup:static-content:deploy
