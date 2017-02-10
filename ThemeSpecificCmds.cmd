0dee30b903c74ecfedc41c7536c27934aa2aff59
VS <VIVEKLUCKY249@users.noreply.github.com>
2017-02-11 04:51:44 +0530

    Update ThemeSpecificCmds.cmd

--- ThemeSpecificCmds.cmd
+++ ThemeSpecificCmds.cmd
@@ -1,1 +1,3 @@
+REM After creating, installing & configuring your custom theme execute below commands in given order
+php bin/magento cache:flush
 php bin/magento setup:static-content:deploy --theme Vender/MyTheme
