63f9df91b19de79299751d8d2b374b77018cefe9
VS <VIVEKLUCKY249@users.noreply.github.com>
2017-02-11 03:47:44 +0530

    Update EnableModCmdSequence.cmd

--- EnableModCmdSequence.cmd
+++ EnableModCmdSequence.cmd
@@ -2,5 +2,6 @@ php bin/magento cache:clean
 php bin/magento cache:flush
 php bin/magento indexer:reindex
 php bin/magento module:enable MagePsycho_Easypathhints --clear-static-content
+php bin/magento module:enable [Namespace]_[Module] --clear-static-content
 php bin/magento setup:upgrade
 php bin/magento setup:static-content:deploy
